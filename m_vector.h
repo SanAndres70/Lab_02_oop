1591c8be3aa05b383846cfeb025e54e8eb0b12cb
SanAndres70 <Andrew70popov@mail.ru>
2025-04-28 23:15:44 +0300

    Update m_vector.h

--- m_vector.h
+++ m_vector.h
@@ -1,116 +1,146 @@
 #ifndef M_VECTOR_H
 #define M_VECTOR_H
 
-#include <initializer_list>
 #include <iostream>
+#include <initializer_list>
+#include <stdexcept>
 #include "iterator.h"
+
+using namespace std;
+
 
 template<typename T>
 class m_vector {
-public:
-    m_vector(int len);
-    m_vector(const T* arr, int len);
-    m_vector(const m_vector<T>& other);
-    m_vector(m_vector<T>&& other);
-    m_vector(std::initializer_list<T> lst);
-
-    ~m_vector();
-
-    int get_length() const;
-    void set_elem(int index, const T& val);
-    T& get_elem(int index);
-    T& operator[](int index);
-
-    T* to_array() const;
-
-    Iterator<T> iterator_begin();
-    Iterator<T> iterator_end();
-
 private:
     T* data;
-    int len;
+    int length;
+
+public:
+    m_vector(int length) : length(length) {
+        if (length <= 0) throw invalid_argument("Length must be positive");
+        data = new T[length]{};
+    }
+
+    m_vector(const m_vector<T>& vect) : length(vect.length) {
+        data = new T[length];
+        for (int i = 0; i < length; ++i) {
+            data[i] = vect.data[i];
+        }
+    }
+
+    m_vector(m_vector<T>&& vect) noexcept : data(vect.data), length(vect.length) {
+        vect.data = nullptr;
+        vect.length = 0;
+    }
+
+    explicit m_vector(initializer_list<T> lst) : length(lst.size()) {
+        data = new T[length];
+        int idx = 0;
+        for (const auto& elem : lst) {
+            data[idx++] = elem;
+        }
+    }
+
+    ~m_vector() {
+        delete[] data;
+    }
+
+    m_vector<T>& operator=(const m_vector<T>& lst) {
+        if (this != &lst) {
+            delete[] data;
+            length = lst.length;
+            data = new T[length];
+            for (int i = 0; i < length; ++i) {
+                data[i] = lst.data[i];
+            }
+        }
+        return *this;
+    }
+
+    int get_length() const {
+        return length;
+    }
+
+    void set_elem(int index, const T& elem) {
+        if (index < 0 || index >= length)
+            throw out_of_range("Index out of range");
+        data[index] = elem;
+    }
+
+    T& get_elem(int index) const {
+        if (index < 0 || index >= length)
+            throw out_of_range("Index out of range");
+        return data[index];
+    }
+
+    T* to_array() {
+        T* arr = new T[length];
+        for (int i = 0; i < length; ++i) {
+            arr[i] = data[i];
+        }
+        return arr;
+    }
+
+    T& operator[](int index) const {
+        if (index < 0 || index >= length)
+            throw out_of_range("Index out of range");
+        return data[index];
+    }
+
+    template<typename _T>
+    friend ostream& operator<<(ostream& os, const m_vector<_T>& lst) {
+        os << "[ ";
+        for (int i = 0; i < lst.length; ++i) {
+            os << lst.data[i] << " ";
+        }
+        os << "]";
+        return os;
+    }
+
+    m_vector<T>& operator+=(const m_vector<T>& vect) {
+        if (length != vect.length)
+            throw invalid_argument("Vectors must have the same length for this operation");
+
+        for (int i = 0; i < length; ++i) {
+            data[i] += vect.data[i];
+        }
+        return *this;
+    }
+
+    m_vector<T>& operator-=(const m_vector<T>& vect) {
+        if (length != vect.length)
+            throw invalid_argument("Vectors must have the same length for this operation");
+
+        for (int i = 0; i < length; ++i) {
+            data[i] -= vect.data[i];
+        }
+        return *this;
+    }
+
+    m_vector<T>& operator*=(const T& val) {
+        for (int i = 0; i < length; ++i) {
+            data[i] *= val;
+        }
+        return *this;
+    }
+
+    m_vector<T>& operator/=(const T& val) {
+        if (val == 0)
+            throw invalid_argument("Division by zero");
+
+        for (int i = 0; i < length; ++i) {
+            data[i] /= val;
+        }
+        return *this;
+    }
+
+    Iterator<T> iterator_begin() {
+        return Iterator<T>(*this, 0);
+    }
+
+    Iterator<T> iterator_end() {
+        return Iterator<T>(*this, length);
+    }
 };
 
-template<typename T>
-m_vector<T>::m_vector(int len) : len(len) {
-    data = new T[len];
-}
-
-template<typename T>
-m_vector<T>::m_vector(const T* arr, int len) : len(len) {
-    data = new T[len];
-    for (int i = 0; i < len; ++i) {
-        data[i] = arr[i];
-    }
-}
-
-template<typename T>
-m_vector<T>::m_vector(const m_vector<T>& other) : len(other.len) {
-    data = new T[len];
-    for (int i = 0; i < len; ++i) {
-        data[i] = other.data[i];
-    }
-}
-
-template<typename T>
-m_vector<T>::m_vector(m_vector<T>&& other) : data(other.data), len(other.len) {
-    other.data = nullptr;
-    other.len = 0;
-}
-
-template<typename T>
-m_vector<T>::m_vector(std::initializer_list<T> lst) : len(lst.size()) {
-    data = new T[len];
-    int i = 0;
-    for (const T& val : lst) {
-        data[i++] = val;
-    }
-}
-
-template<typename T>
-m_vector<T>::~m_vector() {
-    delete[] data;
-}
-
-template<typename T>
-int m_vector<T>::get_length() const {
-    return len;
-}
-
-template<typename T>
-void m_vector<T>::set_elem(int index, const T& val) {
-    if (index < 0 || index >= len) throw std::out_of_range("Index out of bounds");
-    data[index] = val;
-}
-
-template<typename T>
-T& m_vector<T>::get_elem(int index) {
-    if (index < 0 || index >= len) throw std::out_of_range("Index out of bounds");
-    return data[index];
-}
-
-template<typename T>
-T& m_vector<T>::operator[](int index) {
-    return get_elem(index);
-}
-
-template<typename T>
-T* m_vector<T>::to_array() const {
-    T* copy = new T[len];
-    for (int i = 0; i < len; ++i) {
-        copy[i] = data[i];
-    }
-    return copy;
-}
-
-template<typename T>
-Iterator<T> m_vector<T>::iterator_begin() {
-    return Iterator<T>(data);
-}
-
-template<typename T>
-Iterator<T> m_vector<T>::iterator_end() {
-    return Iterator<T>(data + len);
-}
-
-#endif
+#endif // M_VECTOR_H
