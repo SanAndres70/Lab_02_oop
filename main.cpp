772bddb33b7576378693a08eea41d06a41791eff
SanAndres70 <Andrew70popov@mail.ru>
2025-04-29 09:11:56 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,53 @@
-#include "m_vector.h"
-#include "iterator.h"
 #include <iostream>
+#include "m_vector.h"
+
+using namespace std;
 
 int main() {
-    m_vector<int> vec{1, 2, 3, 4, 5};
+    m_vector<int> v1{1, 2, 3, 4, 5};
+    m_vector<int> v2{5, 4, 3, 2, 1};
 
-    cout << "Vector: " << vec << endl;
+    cout << "v1: " << v1 << endl;
+    cout << "v2: " << v2 << endl;
 
-    vec.set_elem(2, 10);
-    cout << "After set_elem: " << vec << endl;
+    m_vector<int> v3 = v1;
+    cout << "v3 (copy of v1): " << v3 << endl;
 
-    int value = vec.get_elem(2);
-    cout << "Value at index 2: " << value << endl;
+    m_vector<int> v4 = std::move(v2);
+    cout << "v4 (moved from v2): " << v4 << endl;
 
-    Iterator<int> it = vec.iterator_begin();
-    while (it != vec.iterator_end()) {
-        cout << it.value() << " ";
-        ++it;
+    v1.set_elem(0, 10);
+    cout << "v1 after set_elem: " << v1 << endl;
+
+    cout << "v1[0] = " << v1.get_elem(0) << endl;
+
+    v1 += v3;
+    cout << "v1 += v3: " << v1 << endl;
+
+    v1 -= v3;
+    cout << "v1 -= v3: " << v1 << endl;
+
+    v1 *= 2;
+    cout << "v1 *= 2: " << v1 << endl;
+
+    v1 /= 2;
+    cout << "v1 /= 2: " << v1 << endl;
+
+    m_vector<int> v5 = v3 + v4;
+    cout << "v3 + v4: " << v5 << endl;
+
+    m_vector<int> v6 = v5 - v3;
+    cout << "v5 - v3: " << v6 << endl;
+
+    m_vector<int> v7 = v3 * 3;
+    cout << "v3 * 3: " << v7 << endl;
+
+    m_vector<int> v8 = v3 / 2;
+    cout << "v3 / 2: " << v8 << endl;
+
+    cout << "Iterating through v3 using Iterator:\n";
+    for (Iterator<int> it = v3.iterator_begin(); !it.is_end(); ++it) {
+        cout << *it << " ";
     }
     cout << endl;
 
